d679c17ecd3bec1acc52d29db2cfdf2d274d22f3
im0o <im0ojiang@outlook.com>
2025-05-11 01:38:04 +0800

    new demo

---
+++ README.md
@@ -0,0 +1,4 @@
+# demo-lightseek
+
+> 本项目作为 [FolioKit](https://github.com/jz0ojiang/Foliokit) 作品集的线上 Demo 展示页，用于展示视觉类网页作品。  
+> This repository serves as an online demo for [FolioKit](https://github.com/jz0ojiang/Foliokit), showcasing a visual storytelling web page.
---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.onload = async () => {
+  var index = 0;
+  var bgvideo = document.getElementById("myVideo");
+  var video = document.getElementById("video1");
+  var audio = document.getElementById("myAudio");
+  bgvideo.onended = function () {
+    if (index) {
+      index = 0;
+      bgvideo.src = "./assets/background.mp4";
+    } else {
+      index = 1;
+      bgvideo.src = "./assets/background2.mp4";
+    }
+  };
+  await audio.play();
+  video.onplay = async () => {
+    await audio.pause();
+  }
+  video.onpause = async () => {
+    await audio.play();
+  }
+
+};
+// 滚动到 屏幕高度以下 时，显示导航栏
+window.onscroll = function () {
+  var top = document.documentElement.scrollTop || document.body.scrollTop;
+  var navbar = document.getElementsByClassName("float-navbar")[0];
+  let navshow = false;
+  if (top >= window.innerHeight - 100) {
+    navbar.style.display = "block";
+    navbar.style.animation = "float-navbar .5s";
+    navbar.onanimationend = () => {
+      navshow = (top >= window.innerHeight)
+    }
+  } else {
+    navbar.style.animation = "float-navbar-out .5s";
+    navbar.onanimationend = () => {
+      navshow = (top >= window.innerHeight)
+      if (!navshow) navbar.style.display = "none";
+    }
+  }
+};
+
+// 点击 #item-show-btn 时，展开/收起 .masonry-box 的 max-height
+
+addEventListener("click", function (e) {
+  if (e.target.id === "item-show-btn") {
+    var box = e.target.parentNode.parentNode;
+    if (box.classList.contains("masonry-box-open")) {
+      box.classList.remove("masonry-box-open");
+      e.target.innerHTML = "展开";
+      // 滚动到锚点 #container-3
+      var top = document.getElementById("container-3").offsetTop;
+      window.scrollTo(0, top);
+    } else {
+      box.classList.add("masonry-box-open");
+      e.target.innerHTML = "收起";
+    }
+  }
+});
